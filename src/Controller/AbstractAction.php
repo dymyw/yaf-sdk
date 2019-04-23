53184ee2a4264f7e5ca4885e9c3380fb3b19a5cc
dymyw <dymayongwei@163.com>
2019-04-23 10:18:48 +0800

    fix

--- src/Controller/AbstractAction.php
+++ src/Controller/AbstractAction.php
@@ -11,9 +11,9 @@ use Yaf\Request_Abstract;
  * @package Dymyw\Yaf\Controller
  * 
  * @method AbstractRequest|Request_Abstract getRequest() 返回 Request 对象
- * @method void success($data = null) 返回成功结果
+ * @method void success(array $data = []) 返回成功结果
  * @method void failed(string $msg, int $code = -1) 返回失败结果
- * @method void response($result) 返回成功结果
+ * @method void response(array $result = []) 返回成功结果
  * @method array getPaginationParams($defaultNum = 1, $defaultLimit = 20) 获取分页参数
  */
 abstract class AbstractAction extends Action_Abstract
